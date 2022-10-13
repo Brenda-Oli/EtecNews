c959cf78ee3cf3662768293ce184cda4b22f44fc
Brenda-Oli <brendaje1@outlook.com>
2022-10-13 13:42:21 -0300

    Model Usuario

---
+++ model/Usuario.php
@@ -0,0 +1,77 @@
+<?php
+
+class Usuario
+{
+    //atributos da tabela usuário
+    public $codusuario;
+    public $nome;
+    public $email;
+    public $senha;
+    public $acesso;
+
+    public function __construct() //será executado ao usar a classe (essa vai ser a primeira coisa que vai ser executada)
+    {
+        include_once "Conexao.php"; //incluindo conexão
+    }
+
+    public function cadastrar()
+    {
+        $con = Conexao::conectar(); //conectar no BD
+        //comando SQL para cadastrar (INSERT)
+        $cmd = $con->prepare("INSERT INTO usuario (nome, email, senha, acesso) 
+        VALUES (:nome, :email, :senha, :acesso)");
+
+        //enviando o valor dos parâmetros
+        $cmd->bindParam(":nome",    $this->nome);
+        $cmd->bindParam(":email",   $this->email);
+        $cmd->bindParam(":senha",   $this->senha);
+        $cmd->bindParam(":acesso",  $this->acesso);
+
+        $cmd->execute(); //executar o comando
+    }
+
+    public function consultar()
+    {
+        $con = Conexao::conectar(); // Primeiro é preciso acessar o BD
+        $cmd = $con->prepare("SELECT * FROM usuario"); // Comando SQL 
+        $cmd->execute(); // Executar o comando
+        return $cmd->fetchAll(PDO::FETCH_OBJ); // Transforma em vetores as coisas / FETCH_OBJ transforma em objeto
+    }
+
+    public function excluir()
+    {
+        $con = Conexao::conectar(); //conectar no BD
+        //comando SQL para excluir (DELETE)
+        $cmd = $con->prepare("DELETE FROM usuario WHERE codusuario = :codusuario");
+
+        //enviando o valor dos parâmetros
+        $cmd->bindParam(":codusuario",    $this->codusuario);
+
+        $cmd->execute(); //executar o comando
+    }
+
+    public function atualizar()
+    {
+        $con = Conexao::conectar(); //conectar no BD
+        //comando SQL para cadastrar (INSERT)
+        $cmd = $con->prepare("UPDATE usuario SET nome = :nome, email = :email, senha = :senha, acesso = :acesso WHERE codusuario = :codusuario");
+        //enviando o valor dos parâmetros
+        $cmd->bindParam(":codusuario", $this->codusuario);
+        $cmd->bindParam(":nome", $this->nome);
+        $cmd->bindParam(":email", $this->email);
+        $cmd->bindParam(":senha", $this->senha);
+        $cmd->bindParam(":acesso", $this->acesso);
+
+        $cmd->execute(); //executar o comando
+    }
+
+    public function retornar()
+    {
+        $con = Conexao::conectar(); //acessar o BD
+        $cmd = $con->prepare("SELECT * FROM usuario
+        WHERE codusuario = :codusuario"); //comando SQL
+        $cmd->bindParam(":codusuario", $this->codusuario);
+        $cmd->execute(); //executar o comando SQL
+        return $cmd->fetch(PDO::FETCH_OBJ);
+    }
+}
